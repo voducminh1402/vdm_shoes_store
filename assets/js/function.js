76db517a3f6c1fb464cc7a898f747052a2934283
voducminh1402 <voducminh140201@gmail.com>
2020-12-28 20:47:28 +0700

    28/12/20

---
+++ assets/js/function.js
@@ -0,0 +1,9 @@
+function onClickFunction() {
+    var x = document.getElementById("menuBar");
+    if (x.style.display === "none") {
+     x.style.display = "block";
+    } 
+    else {
+        x.style.display = "none";
+    }
+}
